0faeb7b019cd1f083ee8dd4a539126ed1b5ad137
Danrem Pena <danrem@multisoft.com>
2019-11-29 22:18:25 +0800

    updates working on queues and dispatcher

--- handlers/sp-runner-handler.js
+++ handlers/sp-runner-handler.js
@@ -2,70 +2,65 @@ import AWS from 'aws-sdk'
 import axios from 'axios'
 import helper from '../lib/helper'
 
-const sqs = new AWS.SQS({region: 'ap-northeast-1'})
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
 const ssm = new AWS.SSM({ apiVersion: '2014-11-06' })
 
+var clientInfo = ''
 
 export const main = (event, context, callback) => {
 
-  const { client : clientInfo, jobs } = event.detail
+  const { client, jobs } = event.detail
+  var AWS_ACCOUNT = context.invokedFunctionArn.split(':')[4]
+  console.log(AWS_ACCOUNT)
+  var QUEUE_URL = `https://sqs.ap-northeast-1.amazonaws.com/${AWS_ACCOUNT}/JobsQueue`
+  clientInfo = client
 
-  var AWS_ACCOUNT = context.invokedFunctionArn.split(":")[4]
-  var QUEUE_URL = `https://sqs.ap-northeast-1.amazonaws.com/${AWS_ACCOUNT}/MyQueue`
+  async function runMPPQuery (jobs) {
 
-  console.log(clientInfo)
-
-// get the data here from MPP SP
-  const accessToken = mppSQLAuthenticate(clientInfo)
-
-  mppSQLAxios.interceptors.request.use(async (opts) => {
-    opts.headers.common['Authorization'] = `Bearer ${accessToken}`
-    return opts
-  }, function (error) {
-    return Promise.reject(error)
-  })
-
-  async function runMPPQuery(jobs){
     await Promise.all(jobs.map(async (job) => {
       const { data } = await mppSQLAxios.post('/queries', {
         query: job.query,
-        options: {
+        /*options: {
           type: 'SELECT'
-        }
+        }*/
       })
 
-      const params = {
-        MessageBody: JSON.stringify(data),
-        QueueUrl: QUEUE_URL
-      }
+      if (Boolean(data) && data.length) {
+        console.log('SP Results Length: ', data.length)
 
-      sqs.sendMessage(params, function(err, data) {
-        if (err) {
-          console.log('error:', 'Fail Send Message' + err);
-
-          const response = {
-            statusCode: 500,
-            body: JSON.stringify({
-              message: 'ERROR'
-            })
-          };
-          callback(null, response)
-        } else {
-          console.log('data:', data)
-          const response = {
-            statusCode: 200,
-            body: JSON.stringify({
-              messageId: data.MesssageId,
-              message: data
-            })
-          };
-          callback(null, response)
+        const params = {
+          MessageBody: JSON.stringify(data),
+          QueueUrl: QUEUE_URL
         }
-      });
+        // console.log(data)
+        sqs.sendMessage(params, function (err, data) {
+          if (err) {
+            console.log('error:', 'Fail Send Message' + err)
+
+            const response = {
+              statusCode: 500,
+              body: JSON.stringify({
+                message: 'ERROR'
+              })
+            }
+            callback(response)
+          } else {
+            console.log('data:', data)
+            const response = {
+              statusCode: 200,
+              body: JSON.stringify({
+                messageId: data.MesssageId,
+                message: data
+              })
+            }
+            callback(null, response)
+          }
+        })
+      }
     }))
   }
 
-  const test = runMPPQuery(jobs)
+  return runMPPQuery(jobs)
 
 }
 
@@ -76,10 +71,10 @@ export const mppSQLAxiosPublic = axios.create({
   }
 })
 
-async function mppSQLAuthenticate(client) {
-  const { ID , secret } = client
+async function mppSQLAuthenticate (client) {
+  const { ID, secret } = client
   const { Parameter: { Value: msaClientId } } = await helper.get_ssm_param(ID)
-  const { Parameter: { Value: msaClientSecret } } =  await helper.get_ssm_param(secret)
+  const { Parameter: { Value: msaClientSecret } } = await helper.get_ssm_param(secret)
   const { data: { accessToken } } = await mppSQLAxiosPublic.post('/authentication', {
     strategy: 'local',
     clientId: msaClientId,
@@ -93,6 +88,14 @@ export const mppSQLAxios = axios.create({
   headers: {
     'Content-Type': 'application/json'
   }
+})
+
+mppSQLAxios.interceptors.request.use(async (opts) => {
+  const accessToken = await mppSQLAuthenticate(clientInfo)
+  opts.headers.common['Authorization'] = 'Bearer ' + accessToken
+  return opts
+}, function (error) {
+  return Promise.reject(error)
 })
 
 export default main
